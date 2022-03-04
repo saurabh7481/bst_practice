218726992df4704b2b6b30d139c56a7fa42adc40
saurabh <saurabh7481@gmail.com>
2022-03-04 15:47:43 +0530

    added preorder traversal

--- index.js
+++ index.js
@@ -58,4 +58,50 @@ class BinarySearchTree{
     return found;
   }
 
+
 }
+
+const bst = new BinarySearchTree();
+bst.insert(25);
+bst.insert(15);
+bst.insert(50);
+bst.insert(10);
+bst.insert(22);
+bst.insert(35);
+bst.insert(70);
+bst.insert(4);
+bst.insert(12);
+bst.insert(18);
+bst.insert(24);
+bst.insert(31);
+bst.insert(44);
+bst.insert(66);
+bst.insert(90);
+
+
+
+const preOrderRecursive = (node) => {
+  if(!node) return;
+
+  console.log(node.value);
+  preOrderRecursive(node.left);
+  preOrderRecursive(node.right);
+}
+
+const preOrderIterative = (node) => {
+  if(!node) return null;
+
+  const stack = [];
+  stack.push(node);
+
+  while(stack.length){
+    const curr = stack.pop();
+    console.log(curr.value);
+
+    if(curr.right) stack.push(curr.right);
+    if(curr.left) stack.push(curr.left);
+  }
+}
+
+preOrderRecursive(bst.root);
+preOrderIterative(bst.root);
